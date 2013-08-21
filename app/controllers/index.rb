9c0a832072e4ccee5459774fc03dc78ebfdfe4c7
cokeefe <chaemobile@gmail.com>
2013-08-20 18:28:49 -0700

    modify form name

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,18 +5,14 @@ end
 
 #----------- SESSIONS -----------
 
+require 'pry'
+
 get '/sessions/new' do
   erb :sign_in
 end
 
 post '/sessions' do
     @user = User.find_by_email(params[:email])
-
-    # line 17 does not work for newly created users.
-    # user object is located, but no method error is prompted:
-    #      undefined method `password' for nil:NilClass
-
-    # when querying with a seeded user, no error
     if @user.password == params[:password]
       session[:logged_in] = @user.id
       redirect '/'
@@ -26,7 +22,6 @@ post '/sessions' do
 end
 
 delete '/sessions/:id' do
-  # sign-out -- invoked via AJAX
   session.clear
   redirect '/sessions/new'
 end
@@ -40,8 +35,9 @@ end
 
 post '/users' do
   new_user = User.create( :name => params[:name],
-                    :email => params["user[email]"])
-  new_user.password = params["user[password]"]
+                    :email => params[:email])
+
+  new_user.password = params[:password]
   new_user.save
 
   check_user = User.find_by_name(params[:name])
